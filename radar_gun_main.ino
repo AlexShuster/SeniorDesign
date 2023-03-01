6ae4bab121f2985c4751ae56a0bc6684ef25e0c0
jimmyschi <55263389+jimmyschi@users.noreply.github.com>
2023-03-01 14:10:19 -0500

    Update radar_gun_main.ino

--- radar_gun_main.ino
+++ radar_gun_main.ino
@@ -1,6 +1,14 @@
+const int velocity_input = 2;
+const int spin_rate_input = 3;
+//Output prediction to a pin, 0 = Offspeed and 1 = Fastball
+const int prediction_output = 4;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(velocity_input,INPUT);
+  pinMode(spin_rate_input,INPUT);
+  pinMode(prediction_output,OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -9,7 +17,7 @@ void loop() {
 }
 
 
-int predict_two__pitch(float velocity, float spin_rate) {
+u_int8_t predict_two__pitch(float velocity, float spin_rate) {
   //W = weight vector from pretrained SVM Model
   float w[2] = {2.68913904, 0.02047686};
   //Bias from pretrained SVM Model
